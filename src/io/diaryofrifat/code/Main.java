a259a003883ae8fbbde3a38372f52898846e89cd
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2021-01-27 21:59:41 +0600

    implement selection sort

--- src/io/diaryofrifat/code/Main.java
+++ src/io/diaryofrifat/code/Main.java
@@ -5,13 +5,33 @@ public class Main {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        doBubbleSort(intArray);
+        doSelectionSort(intArray);
+    }
+
+    private static void doSelectionSort(int[] intArray) {
+        for (int i = intArray.length - 1; i > 0; i--) {
+            int largest = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (intArray[j] > intArray[largest]) { // (>) : Small to Large & (<) : Large to Small
+                    largest = j;
+                }
+            }
+
+            swap(intArray, i, largest);
+        }
+
+        printArray(intArray);
     }
 
     private static void doBubbleSort(int[] intArray) {
         doBubbleSortApproachOne(intArray);
         //doBubbleSortApproachTwo(intArray);
 
+        printArray(intArray);
+    }
+
+    private static void printArray(int[] intArray) {
         for (int j : intArray) {
             System.out.print(j + " ");
         }
