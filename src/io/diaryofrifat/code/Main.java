6fef1730384c41a6dc52d78ea83544026754a267
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2021-01-27 17:03:09 +0600

    implement variations of bubble sort

--- src/io/diaryofrifat/code/Main.java
+++ src/io/diaryofrifat/code/Main.java
@@ -9,16 +9,33 @@ public class Main {
     }
 
     private static void doBubbleSort(int[] intArray) {
+        doBubbleSortApproachOne(intArray);
+        //doBubbleSortApproachTwo(intArray);
+
+        for (int j : intArray) {
+            System.out.print(j + " ");
+        }
+    }
+
+    private static void doBubbleSortApproachOne(int[] intArray) {
+        // Pushing the largest or the smallest number on the right side
         for (int i = intArray.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (intArray[j] > intArray[j + 1]) {
+                if (intArray[j] < intArray[j + 1]) { // (>) : Small to Large & (<) : Large to Small
                     swap(intArray, j, j + 1);
                 }
             }
         }
+    }
 
-        for (int j : intArray) {
-            System.out.print(j + " ");
+    private static void doBubbleSortApproachTwo(int[] intArray) {
+        // Pushing the largest or the smallest number on the left side
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = intArray.length - 1; j > i; j--) {
+                if (intArray[j] < intArray[j - 1]) { // (<) : Small to Large & (>) : Large to Small
+                    swap(intArray, j, j - 1);
+                }
+            }
         }
     }
 
